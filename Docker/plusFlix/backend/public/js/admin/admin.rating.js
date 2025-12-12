9a08da434b0effbf5318570da52b277f339673b1
Bartek <bartekszewczyk2003@gmail.com>
2025-12-12 10:22:16 +0100

    CSS for category and rating dones. Ratings done.

---
+++ Docker/plusFlix/backend/public/js/admin/admin.rating.js
@@ -0,0 +1,43 @@
+const modal = document.getElementById('ratingModal');
+const closeBtn = document.querySelector('.modal-rating-close');
+const form = document.getElementById('ratingForm');
+const title = document.getElementById('modalTitle');
+const ratingInput = document.getElementById('ratingID');
+const commentInput = document.getElementById('commentID');
+
+document.querySelectorAll('.editratingLink').forEach(link => {
+    link.onclick = () => {
+        modal.style.display = 'block';
+        title.innerText = link.dataset.name;
+
+        const ratingId = link.dataset.idRating;
+
+        let rating = link.dataset.rating;
+        let comment = link.dataset.comment;
+
+        ratingInput.value = rating;
+        commentInput.value = comment;
+
+        form.action = "/admin/rating/edit/" + ratingId;
+    };
+});
+
+document.querySelectorAll('.deleteratingLink').forEach(link => {
+    link.onclick = (e) => {
+        e.preventDefault();
+        if (!confirm('Na pewno chcesz usunąć tę kategorię?')) return;
+        const ratingId = link.dataset.idRating;
+
+        let form = document.createElement('form');
+        form.method = 'POST';
+        form.action = '/admin/rating/delete/' + ratingId;
+        document.body.appendChild(form);
+
+        form.submit();
+    }
+})
+
+closeBtn.onclick = () => modal.style.display = 'none';
+window.onclick = (e) => {
+    if (e.target === modal) modal.style.display = 'none';
+};
